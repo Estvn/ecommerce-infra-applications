508cfe814ad4da940e78ada49768deebb74bd649
Estvn <ir51370@gmail.com>
2025-06-20 21:24:40 -0600

    Modificando la organizacióon de los recursos serverless

--- serverless.tf
+++ serverless.tf
@@ -1,35 +1,23 @@
-# Configuración del Queue Storage de Azure
-resource "azurerm_storage_account" "sa_queue" {
-    name                     = "saqueue${lower(var.project_name)}${lower(var.enviroment)}"
+# Creando un Storage Account para Azure Functions y Queue Storage
+resource "azurerm_storage_account" "sa_serverless" {
+    name                     = "saserverless${lower(var.project_name)}${lower(var.enviroment)}"
     resource_group_name      = azurerm_resource_group.rg_ecommerce.name
     location                 = var.location
     
-    account_kind             = "StorageV2"
     account_tier             = "Standard"
     account_replication_type = "LRS"
     tags                      = var.tags
 }
 
-resource "azurerm_storage_queue" "qs" {
-    name                 = "qs-${lower(var.project_name)}-${lower(var.enviroment)}"
-    storage_account_name = azurerm_storage_account.sa_queue.name
+# Creando el Queue Storage
+resource "azurerm_storage_queue" "queue" {
+    name                 = "queue-${lower(var.project_name)}-${lower(var.enviroment)}"
+    storage_account_name = azurerm_storage_account.sa_serverless.name
 }
 
-# Configuración de Azure Functions 
-resource "azurerm_storage_account" "sa_functions" {
-    name                     = "safunctions${lower(var.project_name)}${lower(var.enviroment)}"
-    resource_group_name      = azurerm_resource_group.rg_ecommerce.name
-    location                 = var.location
-
-    account_kind             = "StorageV2"
-    account_tier             = "Standard"
-    account_replication_type = "LRS"
-        
-    tags                      = var.tags
-}
-
-resource "azurerm_service_plan" "sp_functions" {
-    name                = "sp-functions-${lower(var.project_name)}-${lower(var.enviroment)}"
+# Creando un Service Plan para Azure Functions
+resource "azurerm_service_plan" "sp_serverless" {
+    name                = "sp-serverless-${lower(var.project_name)}-${lower(var.enviroment)}"
     resource_group_name = azurerm_resource_group.rg_ecommerce.name
     location            = var.location
 
@@ -38,14 +26,16 @@ resource "azurerm_service_plan" "sp_functions" {
     tags                = var.tags
 }
 
-resource "azurerm_linux_function_app" "fa" {
-    name               = "fa-${lower(var.project_name)}-${lower(var.enviroment)}"
+# Creando una Azure Function
+# Azure Functions comparte el mismo Storage Account que el Queue Storage
+resource "azurerm_linux_function_app" "fa_serveless" {
+    name               = "fa-serverless-${lower(var.project_name)}-${lower(var.enviroment)}"
     resource_group_name = azurerm_resource_group.rg_ecommerce.name
     location           = var.location
 
-    storage_account_name       = azurerm_storage_account.sa_functions.name
-    storage_account_access_key = azurerm_storage_account.sa_functions.primary_access_key
-    service_plan_id            = azurerm_service_plan.sp_functions.id
+    storage_account_name       = azurerm_storage_account.sa_serverless.name
+    storage_account_access_key = azurerm_storage_account.sa_serverless.primary_access_key
+    service_plan_id            = azurerm_service_plan.sp_serverless.id
 
     tags                       = var.tags    
     site_config {}
